12e2c2ba8e203e6ed8b139450a6a83b030ab0deb
Jonas Reinstaedtler <j.reinstaedtler@tum.de>
2025-05-10 00:06:56 +0200

    initialized empty springboot project

---
+++ gh-connector/src/main/java/de/tum/gh_connector/GhConnectorApplication.java
@@ -0,0 +1,13 @@
+package de.tum.gh_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhConnectorApplication.class, args);
+	}
+
+}
---
+++ gh-connector/src/test/java/de/tum/gh_connector/GhConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tum.gh_connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
