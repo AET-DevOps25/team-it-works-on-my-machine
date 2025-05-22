9f502bace468e6f03ce7c1ffdd4a5ef46caf0278
Xiyu <767145420@qq.com>
2025-05-22 17:10:38 +0200

    Fix URL in ask.py and update port mapping in docker-compose.yml

--- genai/app/input_query/ask.py
+++ genai/app/input_query/ask.py
@@ -1,7 +1,7 @@
 import requests
 
 if __name__ == "__main__":
-    url = "http://localhost:8001/ask"
+    url = "http://localhost:8000/ask"
 
     response = requests.post(url, json={"question": "What is the Rank of TUM?"})
     print(response.json()["response"])
