34aadaf022230e42d75491ef0bd8542cc5e4b610
Felix Schnabel <f.schnabel@tum.de>
2025-07-11 22:35:14 +0200

    Add analysis model to the users microservice

--- users/src/main/java/de/tum/users/controller/UserController.java
+++ users/src/main/java/de/tum/users/controller/UserController.java
@@ -1,8 +1,11 @@
 package de.tum.users.controller;
 
+import de.tum.users.model.Analysis;
 import de.tum.users.model.User;
+import de.tum.users.repository.AnalysisRepository;
 import de.tum.users.repository.UserRepository;
 import java.util.List;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,16 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@Log4j2
 public class UserController {
     private final UserRepository userRepository;
+    private final AnalysisRepository analysisRepository;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, AnalysisRepository analysisRepository) {
         this.userRepository = userRepository;
+        this.analysisRepository = analysisRepository;
     }
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return userRepository.findAll().stream()
+                .map(user -> {
+                    user.setId(user.getId().substring(0, Math.min(user.getId().length(), 4)) + "...");
+                    user.setToken(user.getToken()
+                                    .substring(0, Math.min(user.getToken().length(), 4)) + "...");
+                    return user;
+                })
+                .toList();
     }
 
     @GetMapping("/users/{id}")
@@ -48,10 +61,39 @@ public class UserController {
         return newUser.getId();
     }
 
-    @DeleteMapping("/users/{githubId}")
+    @PostMapping(value = "/users/{id}/analysis", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createAnalysis(@PathVariable String id, @RequestBody Analysis analysis) {
+        Analysis analysisNew = userRepository
+                .findById(id)
+                .map(existingUser -> {
+                    log.info("Adding analysis to user: {}", existingUser.getUsername());
+                    log.info("Analysis: {}", analysis);
+                    existingUser.getAnalysis().add(analysis);
+                    analysis.setUser(existingUser);
+                    userRepository.saveAndFlush(existingUser);
+                    return analysisRepository.saveAndFlush(analysis);
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with id: " + id));
+        return analysisNew.getId();
+    }
+
+    @DeleteMapping("/users/{userId}/analysis/{analysisId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable String githubId) {
-        User user = userRepository.findByGithubId(githubId).orElseThrow(() -> new RuntimeException("User not found"));
+    public void deleteAnalysis(@PathVariable String userId, @PathVariable String analysisId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        Analysis analysis = user.getAnalysis().stream()
+                .filter(a -> a.getId().equals(analysisId))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Analysis not found"));
+        user.getAnalysis().remove(analysis);
+        userRepository.save(user);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable String userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
         userRepository.delete(user);
     }
 
---
+++ users/src/main/java/de/tum/users/model/Analysis.java
@@ -0,0 +1,48 @@
+package de.tum.users.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "analysis")
+public class Analysis {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "repository")
+    private String repository;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
+    protected Analysis() {
+        // no-args constructor required by JPA spec
+        // this one is protected since it shouldn’t be used directly
+    }
+
+    public Analysis(User user, String content, String repository) {
+        this.user = user;
+        this.content = content;
+        this.repository = repository;
+    }
+}
