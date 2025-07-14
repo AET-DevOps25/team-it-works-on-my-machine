a98734ec150e90a0d0fd9b7dbf5fbd6ba1bb81e3
Jonas Reinstaedtler <j.reinstaedtler@tum.de>
2025-07-14 20:17:43 +0200

    add custom prometheus image and container

--- prometheus/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Expand environment variables into prometheus.yml
-envsubst < /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.yml
-
-# Run Prometheus
-exec /bin/prometheus --config.file=/etc/prometheus/prometheus.yml
