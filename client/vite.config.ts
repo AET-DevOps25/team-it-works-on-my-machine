6c0f8f09a12279fb6aefa98622eaff3dca1fb5b7
Felix Schnabel <f.schnabel@tum.de>
2025-07-20 15:10:16 +0200

    Remove rollup visualizer

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig, loadEnv, type PluginOption } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [
-      react(),
-      tailwindcss(),
-      visualizer({ template: 'network' }) as PluginOption,
-    ],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'),
